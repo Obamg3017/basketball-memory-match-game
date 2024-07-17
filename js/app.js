be4d1bb7147d427e8958fe66599bc0212756d2c9
FemiBam <Femibam1@gmail.com>
2024-07-17 10:11:12 -0400

    memoryBoardComplete

---
+++ js/app.js
@@ -0,0 +1,60 @@
+//Setup initial variables and constants:
+
+//DECLARE board as an array
+//DECLARE turn as string
+//DECLARE moves as integer
+//DECLARE matchedPairs as integer
+//DECLARE IMAGES as array of NBA player images
+//DECLARE board as an array
+//DECLARE TOTAL_PAIRS as integer equal to the number of pairs needed
+
+//Cache DOM elements:
+
+//DECLARE startButton
+//DECLARE resetButton
+//DECLARE messageEl
+//DECLARE moveCounterEl
+//DECLARE gameBoardEl
+//DECLARE cardEl
+
+//Event Listeners:
+
+//ADD event listener to startButton for click to startGame function
+//ADD event listener to resetButton to resetGame function
+//ADD event listener to each cardEl for click to handle card click function
+
+//Start Game Functionality:
+
+//Function startGame
+//Function initGame
+//Function loadImages
+//Function Random-Shuffle Images
+
+//Rendering the Game Board:
+//Function RenderBoard
+//Loop through board array
+
+//Handle Card Clicks & Matching Logic:
+
+//Conditionals to check IF card was clicked to flip card
+//Else do not flip card
+//Check for match
+
+//End Game & Reset Functionality:
+
+//Function End Game
+//check for winner
+//check for loser
+
+//Function Reset Game
+
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
